9c8850595bb6493bdb1c1c36fa7f1f3b9c796e07
ainturias <alex.universidad18@gmail.com>
2024-11-20 23:09:33 -0400

    Creacion de los modelos

---
+++ app/Models/Client.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    // Especifica el nombre de la tabla en la base de datos
+    protected $table = 'client';
+
+    // Campos que pueden ser llenados masivamente
+    protected $fillable = [
+        'userId',
+        'address',
+    ];
+
+    /**
+     * Relación uno a uno con la tabla `users`.
+     * Un cliente está asociado a un usuario.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+
+    /**
+     * Relación uno a muchos con la tabla `pet`.
+     * Un cliente puede tener muchas mascotas.
+     */
+    public function pets()
+    {
+        return $this->hasMany(Pet::class, 'clientId');
+    }
+
+    /**
+     * Relación uno a muchos con la tabla `appointment`.
+     * Un cliente puede tener varias citas médicas.
+     */
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class, 'clientId');
+    }
+}
